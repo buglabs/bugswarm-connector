f9cf839c14e0461eb677edafc0de2efbe523ffe3
Ken Gilmer <kgilmer@gmail.com>
2011-04-12 10:33:30 +0900

    bugswarm-connector: test stub for swarm connectivity tests.

---
+++ test/com/bug/abs/bug/swarm/connector/test/BasicConnectivityTests.java
@@ -0,0 +1,63 @@
+package com.bug.abs.bug.swarm.connector.test;
+
+import junit.framework.TestCase;
+
+/**
+ * See RMI http://redmine/issues/2312
+ * 
+ * @author kgilmer
+ *
+ */
+public class BasicConnectivityTests extends TestCase {
+
+	/**
+	 * Connect to a swarm server.
+	 */
+	public void testConnectToSwarmServer() {
+		
+	}
+	
+	/**
+	 * Join a pre-existing swarm
+	 */
+	public void testJoinAvailableSwarm() {
+		
+	}
+	
+	/**
+	 * Create and join a new swarm.
+	 * 
+	 * See https://github.com/buglabs/bugswarm/wiki/Swarms-API
+	 */
+	public void testJoinNewSwarm() {
+		
+	}
+	
+	/**
+	 * Respond to a swarm server admin message over chat for content.
+	 */
+	public void testRespondToXMPPadminChatMessage() {
+		
+	}
+	
+	/**
+	 * Respond to MUC invitation request from swarm server.
+	 */
+	public void testRespondToXMPPMUCRoomInvitation() {
+		
+	}
+	
+	/**
+	 * Respond to swarm server request for binary content with HTTP POST of requested data.
+	 */
+	public void testRespondToXMPPBinaryChatMessage() {
+		
+	}
+	
+	/**
+	 * Respond to a swarm server message over chat for content.
+	 */
+	public void testRespondToXMPPMUCChatMessage() {
+		
+	}
+}

5f8747d313b2023b4fbf400d6715d6f189e2f0f4
Ken Gilmer <kgilmer@gmail.com>
2011-04-13 18:43:12 +0900

    bugswarm-connector: basic activator that listens for cm config changes.  Based on xmpp bugswarm prototype.

--- src/com/buglabs/bug/swarm/connector/Activator.java
+++ src/com/buglabs/bug/swarm/connector/Activator.java
@@ -1,14 +1,44 @@
 package com.buglabs.bug.swarm.connector;
 
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.osgi.service.log.LogService;
 
-public class Activator implements BundleActivator {
+import com.buglabs.util.LogServiceUtil;
+
+public class Activator implements BundleActivator, ManagedService {
+	public static final String CONFIG_PID_BUGSWARM = "BUGSWARM";
+	public static final String CONFIG_KEY_BUGSWARM_ENABLED = "bugdash.swarm.boolean.enabled";
+	public static final String CONFIG_KEY_BUGSWARM_SERVER = "bugdash.swarm.string.serverurl";
+	public static final String CONFIG_KEY_BUGSWARM_NICKNAME = "bugdash.swarm.string.nickname";
+	public static final String CONFIG_KEY_BUGSWARM_USERKEY = "bugdash.swarm.string.userkey";
+	public static final String SWARM_API_KEY_PROPERTY_NAME = "com.buglabs.swarm.user.key";
 
 	private static BundleContext context;
+	private static LogService log;
+	
+	private Dictionary cachedConfig;
+	private ServiceRegistration cmSr;
 
+	/**
+	 * @return BundleContext
+	 */
 	static BundleContext getContext() {
 		return context;
+	}
+	
+	/**
+	 * @return LogService
+	 */
+	public static LogService getLog() {
+		return log;
 	}
 
 	/*
@@ -17,6 +47,8 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		log = LogServiceUtil.getLogService(context);
+		cmSr = context.registerService(ManagedService.class.getName(), this, getCMDictionary());
 	}
 
 	/*
@@ -25,6 +57,52 @@ public class Activator implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+		log = null;
+		cmSr.unregister();
+	}
+	
+	/**
+	 * @return A dictionary defining the configurations we're interested in.
+	 */
+	private Dictionary getCMDictionary() {
+		Dictionary hm = new Hashtable();
+		
+		hm.put("service.pid", CONFIG_PID_BUGSWARM);
+		
+		return hm;
+	}
+
+	@Override
+	public void updated(Dictionary config) throws ConfigurationException {
+		if (config == null || (cachedConfig != null && configsIdentical(cachedConfig, config))) {
+			//Nothing has changed, ignore event.
+			return;
+		}
+		
+		throw new RuntimeException("Implement me!");
+	}
+	
+	/**
+	 * Calling .equals on these with identical kvps returns false.  Doing it the hard way.
+	 * @param c1
+	 * @param c2
+	 * @return
+	 */
+	private boolean configsIdentical(Dictionary c1, Dictionary c2) {
+		if (c1.size() != c2.size()) {
+			return false;
+		}
+		
+		for (Enumeration e = c1.keys(); e.hasMoreElements();) {
+			Object c1k = e.nextElement();
+			Object c1v = c1.get(c1k);
+			
+			if (c2.get(c1k) != c1v) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 
 }

c79b0475e542edd5e07c95f702c88d888f1c65d0
Ken Gilmer <kgilmer@gmail.com>
2011-04-21 09:22:42 +0900

    bugswarm-connector: added class to create xml messages

---
+++ src/com/buglabs/bug/swarm/connector/XmlMessageCreator.java
@@ -0,0 +1,25 @@
+package com.buglabs.bug.swarm.connector;
+
+import java.util.List;
+
+import com.buglabs.util.XmlNode;
+
+/**
+ * This stateless class handles all xml message creation for bugswarm-connector.
+ * @author kgilmer
+ *
+ */
+public class XmlMessageCreator {
+
+	/**
+	 * See https://github.com/buglabs/bugswarm/wiki/Advertise-Member-Capabilities (step 6)
+	 * 
+	 * @param services
+	 * @param modules
+	 * @param feeds
+	 * @return
+	 */
+	public static XmlNode createServiceModuleFeedDocument(List services, List modules, List feeds) {
+		return null;
+	}
+}

752b7f5d3f4721de90710a69d5bd0a81e6e5e4d2
Ken Gilmer <kgilmer@gmail.com>
2011-04-22 10:10:58 +0900

    bugswarm-connector: add mock code to allow for junit testing without osgi runtime context.

--- src/com/buglabs/bug/swarm/connector/OSGiHelper.java
+++ src/com/buglabs/bug/swarm/connector/OSGiHelper.java
@@ -1,13 +1,18 @@
 package com.buglabs.bug.swarm.connector;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceListener;
 
 import com.buglabs.module.IModuleControl;
+import com.buglabs.module.IModuleProperty;
+import com.buglabs.services.ws.IWSResponse;
+import com.buglabs.services.ws.PublicWSDefinition;
 import com.buglabs.services.ws.PublicWSProvider;
 import com.buglabs.util.OSGiServiceLoader;
 
@@ -29,6 +34,10 @@ public class OSGiHelper implements ServiceListener {
 	public static OSGiHelper getRef() throws Exception {
 		if (ref == null)
 			ref = new OSGiHelper();
+		
+		//Here we are checking to see if the context is null.  If so we are not running in OSGi and need to provide
+		//some fake data for test execution.  This should likely be disabled in production code, or when
+		//osgi-based junits are available.	
 
 		return ref;
 	}
@@ -60,15 +69,19 @@ public class OSGiHelper implements ServiceListener {
 			moduleList = new ArrayList<IModuleControl>();
 		else 
 			moduleList.clear();
-
-		synchronized (moduleList) {
-			OSGiServiceLoader.loadServices(context, IModuleControl.class.getName(), null, new OSGiServiceLoader.IServiceLoader() {
-				public void load(Object service) throws Exception {
-					if (!moduleList.contains(service)) {
-						moduleList.add((IModuleControl) service);
+		
+		if (context != null) {
+			synchronized (moduleList) {
+				OSGiServiceLoader.loadServices(context, IModuleControl.class.getName(), null, new OSGiServiceLoader.IServiceLoader() {
+					public void load(Object service) throws Exception {
+						if (!moduleList.contains(service)) {
+							moduleList.add((IModuleControl) service);
+						}
 					}
-				}
-			});
+				});
+			}
+		} else {
+			loadMockIModuleControls();
 		}
 	}
 
@@ -86,17 +99,23 @@ public class OSGiHelper implements ServiceListener {
 		else 
 			serviceList.clear();
 	
-		synchronized (serviceList) {
-			OSGiServiceLoader.loadServices(context, PublicWSProvider.class.getName(), null, new OSGiServiceLoader.IServiceLoader() {
-				public void load(Object service) throws Exception {
-					if (!serviceList.contains(service)) {
-						serviceList.add((PublicWSProvider) service);
+		if (context != null) {
+			synchronized (serviceList) {
+				OSGiServiceLoader.loadServices(context, PublicWSProvider.class.getName(), null, new OSGiServiceLoader.IServiceLoader() {
+					public void load(Object service) throws Exception {
+						if (!serviceList.contains(service)) {
+							serviceList.add((PublicWSProvider) service);
+						}
 					}
-				}
-			});
+				});
+			}
+		} else {
+			loadMockPublicWSProviders();
 		}
 	}
 
+
+	
 	/**
 	 * Clean up and stop listening to OSGi service registry.
 	 */
@@ -140,7 +159,7 @@ public class OSGiHelper implements ServiceListener {
 			}
 			
 			try {
-			initializeWSProviders();
+				initializeWSProviders();
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
@@ -158,5 +177,134 @@ public class OSGiHelper implements ServiceListener {
 		boolean classValid = event.getSource() instanceof IModuleControl || event.getSource() instanceof PublicWSProvider;
 		return typeValid && classValid;
 	}
+	
+	
+	////////////// Test code follows, can be removed for production
+	
+	private void loadMockIModuleControls() {
+		moduleList.add(new MockIModuleControl("GPS", 1, createMockProperties()));
+		moduleList.add(new MockIModuleControl("LCD", 2, createMockProperties()));
+		moduleList.add(new MockIModuleControl("CAMERA", 3, createMockProperties()));
+	}
+	
+	private List createMockProperties() {
+		List l = new ArrayList();
+		final Random r = new Random();
+		l.add(new IModuleProperty() {
+			
+			@Override
+			public boolean isMutable() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public Object getValue() {
+			
+				return "" + r.nextDouble();
+			}
+			
+			@Override
+			public String getType() {
+				// TODO Auto-generated method stub
+				return "String";
+			}
+			
+			@Override
+			public String getName() {
+				// TODO Auto-generated method stub
+				return "" + r.nextDouble();
+			}
+		});
+		
+		return l;
+	}
 
+	private void loadMockPublicWSProviders() {
+		serviceList.add(new MockPublicWSProvider("Picture", "Take a picture using the camera module."));
+		serviceList.add(new MockPublicWSProvider("Location", "Determine your location using GPS services."));
+	}
+
+	private class MockPublicWSProvider implements PublicWSProvider {
+
+		private final String name;
+		private final String desc;
+
+		public MockPublicWSProvider(String name, String desc) {
+			this.name = name;
+			this.desc = desc;
+			
+		}
+		
+		@Override
+		public PublicWSDefinition discover(int operation) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public IWSResponse execute(int operation, String input) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getPublicName() {
+			return name;
+		}
+
+		@Override
+		public String getDescription() {
+			return desc;
+		}
+		
+	}
+	
+	private class MockIModuleControl implements IModuleControl {
+
+		private final String name;
+		private final int slot;
+		private final List properties;
+
+		MockIModuleControl(String name, int slot, List properties) {
+			this.name = name;
+			this.slot = slot;
+			this.properties = properties;
+			
+		}
+		
+		@Override
+		public List getModuleProperties() {
+			return properties;
+		}
+
+		@Override
+		public String getModuleName() {			
+			return name;
+		}
+
+		@Override
+		public int getSlotId() {			
+			return slot;
+		}
+
+		@Override
+		public boolean setModuleProperty(IModuleProperty property) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public int suspend() throws IOException {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int resume() throws IOException {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
 }

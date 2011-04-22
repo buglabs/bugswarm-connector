dfecabfc992807101d199badbb8f4ece6d724c91
Ken Gilmer <kgilmer@gmail.com>
2011-04-22 10:34:39 +0900

    bugswarm-connector: fix up some test cases.

--- test/com/bug/abs/bug/swarm/connector/test/SwarmMemberWSAPITests.java
+++ test/com/bug/abs/bug/swarm/connector/test/SwarmMemberWSAPITests.java
@@ -16,17 +16,27 @@ public class SwarmMemberWSAPITests extends BaseWSAPICase {
 	private static final String DEFAULT_TEST_USER = "testUser";
 	private static final String DEFAULT_RESOURCE_STRING = "Psi";
 	private static final MemberType DEFAULT_MEMBER_TYPE = MemberType.CONSUMER;
-
-	public void testListConsumerMembers() throws IOException {
+	
+	/**
+	 * This test must occur after testSwarmId is set by a previous test.
+	 * @throws IOException 
+	 */
+	public void testAddSwarmMember() throws IOException {
 		ISwarmWSClient client = new SwarmWSClient(SWARM_HOST, API_KEY);
 		IMembersClient membersClient = ((SwarmWSClient) client).getMembers();
 		
 		String id = client.create(getTestSwarmName(), true, getTestSwarmDescription());
-		
-		assertNotNull(id);
-		assertTrue(id.length() > 0);
-		
 		testSwarmId = id;
+		
+		//TODO: determine set of test users that can be created or assumed to exist.
+		int rc = membersClient.add(id, DEFAULT_MEMBER_TYPE, DEFAULT_TEST_USER, DEFAULT_RESOURCE_STRING);
+
+		assertTrue(rc == 201);
+	}
+
+	public void testListConsumerMembers() throws IOException {
+		ISwarmWSClient client = new SwarmWSClient(SWARM_HOST, API_KEY);
+		IMembersClient membersClient = ((SwarmWSClient) client).getMembers();
 		
 		List<SwarmMemberModel> list = membersClient.list(testSwarmId, MemberType.CONSUMER);
 		
@@ -50,42 +60,10 @@ public class SwarmMemberWSAPITests extends BaseWSAPICase {
 		List<SwarmMemberModel> list = membersClient.list(testSwarmId, MemberType.PRODUCER);
 		
 		assertNotNull(list);
-		assertTrue(list.size() > 0);
+		//There should be no PRODUCERS since we created only a CONSUMER
+		assertTrue(list.size() == 0);
 		
 		//TODO validate that a we are a member of the swarm since we created it.
-	}
-	
-	public void testListAllMembers() throws IOException {
-		ISwarmWSClient client = new SwarmWSClient(SWARM_HOST, API_KEY);
-		IMembersClient membersClient = ((SwarmWSClient) client).getMembers();
-		
-		String id = client.create(getTestSwarmName(), true, getTestSwarmDescription());
-		
-		assertNotNull(id);
-		assertTrue(id.length() > 0);
-		
-		testSwarmId = id;
-		
-		List<SwarmMemberModel> list = membersClient.list(testSwarmId, MemberType.CONSUMER);
-		
-		assertNotNull(list);
-		assertTrue(list.size() > 0);
-		
-		//TODO validate that a we are a member of the swarm since we created it.
-	}
-	
-	/**
-	 * This test must occur after testSwarmId is set by a previous test.
-	 * @throws IOException 
-	 */
-	public void testAddSwarmMember() throws IOException {
-		ISwarmWSClient client = new SwarmWSClient(SWARM_HOST, API_KEY);
-		IMembersClient membersClient = ((SwarmWSClient) client).getMembers();
-		
-		//TODO: determine set of test users that can be created or assumed to exist.
-		int rc = membersClient.add(testSwarmId, DEFAULT_MEMBER_TYPE, DEFAULT_TEST_USER, DEFAULT_RESOURCE_STRING);
-
-		assertTrue(rc == 200);
 	}
 	
 	/**

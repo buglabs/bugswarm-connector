0c4985f0a079ad5578880c5612c1036bc6b83c1b
Ken Gilmer <kgilmer@gmail.com>
2011-04-25 09:45:31 +0900

    bugswarm-connector: client should connect and advertise static service and module configuration to peers.  Untested.

--- src/com/buglabs/bug/swarm/connector/BUGSwarmConnector.java
+++ src/com/buglabs/bug/swarm/connector/BUGSwarmConnector.java
@@ -1,11 +1,20 @@
 package com.buglabs.bug.swarm.connector;
 
-import java.io.IOException;
-import java.util.Dictionary;
+import java.util.List;
+
+import org.jivesoftware.smack.Connection;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smackx.muc.InvitationListener;
+import org.jivesoftware.smackx.muc.MultiUserChat;
 
 import com.buglabs.bug.swarm.connector.osgi.OSGiHelper;
+import com.buglabs.bug.swarm.connector.ws.IMembersClient.MemberType;
+import com.buglabs.bug.swarm.connector.ws.SwarmMemberModel;
+import com.buglabs.bug.swarm.connector.ws.SwarmModel;
 import com.buglabs.bug.swarm.connector.ws.SwarmWSClient;
 import com.buglabs.bug.swarm.connector.xmpp.SwarmXMPPClient;
+import com.buglabs.bug.swarm.connector.xmpp.XmlMessageCreator;
+import com.buglabs.util.XmlNode;
 
 /**
  * The swarm connector client for BUGswarm system.
@@ -28,6 +37,7 @@ public class BUGSwarmConnector extends Thread {
 	 */
 	private boolean initialized = false;
 	private SwarmXMPPClient xmppClient;
+	private OSGiHelper osgiHelper;
 
 	public BUGSwarmConnector(Configuration config) {
 		this.config = config;
@@ -38,11 +48,36 @@ public class BUGSwarmConnector extends Thread {
 	@Override
 	public void run() {
 		try {
+			// Initialize the clients used to communicate with swarm server
 			if (!initialized)
 				initialize();
 			
+			//Load data about server configuration and local configuration.
+			List<SwarmModel> allSwarms = wsClient.getMembers().getSwarmsByMember(config.getUsername());
+			XmlNode document = XmlMessageCreator.createServiceModuleFeedDocument(
+					osgiHelper.getBUGServices(), 
+					osgiHelper.getBUGModules(), 
+					osgiHelper.getBUGFeeds());
+			
+			//Notify all swarms of presence.
+			for (SwarmModel swarm: allSwarms)
+				 xmppClient.joinSwarm(swarm.getId());
+			
+			//Notify all consumer-members of swarms of services, feeds, and modules.
+			for (SwarmModel swarm: allSwarms) 
+				for (SwarmMemberModel member: swarm.getMembers())
+					if (member.getType() == MemberType.CONSUMER &&  xmppClient.isPresent(swarm.getId(), member.getUserId()))
+						xmppClient.advertise(
+								swarm.getId(), 
+								member.getUserId(), 
+								document);
+
+			//Listen for invites from swarms
+			MultiUserChat.addInvitationListener(xmppClient.getConnection(), new SwarmInvitationListener());
+			
 			
 		} catch (Exception e) {
+			e.printStackTrace();
 			//TODO handle errors
 		}
 	}
@@ -55,13 +90,30 @@ public class BUGSwarmConnector extends Thread {
 		wsClient = new SwarmWSClient(config.getHostname(), config.getApi_key());
 		if (wsClient.isValid()) {
 			xmppClient = new SwarmXMPPClient(config);
+			xmppClient.connect();
 			
-			if (OSGiHelper.getRef() != null) {
+			osgiHelper = OSGiHelper.getRef();
+			if (osgiHelper != null) {
 				initialized = true;
 				return true;
 			}
 		}
 		
 		return false;
+	}
+	
+	/**
+	 * Class to handle PMs from other clients.
+	 * @author kgilmer
+	 *
+	 */
+	private class SwarmInvitationListener implements InvitationListener {
+
+		@Override
+		public void invitationReceived(Connection conn, String room, String inviter, String reason, String password, Message message) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 }

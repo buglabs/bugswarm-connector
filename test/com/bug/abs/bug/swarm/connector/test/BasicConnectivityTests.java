0c3bb503629f1d115ef55c36c6a149ee60d2cec1
Ken Gilmer <kgilmer@gmail.com>
2011-06-06 09:36:12 +0900

    bugswarm-connector: updated tests

--- test/com/bug/abs/bug/swarm/connector/test/BasicConnectivityTests.java
+++ test/com/bug/abs/bug/swarm/connector/test/BasicConnectivityTests.java
@@ -1,5 +1,6 @@
 package com.bug.abs.bug.swarm.connector.test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +13,13 @@ import org.json.simple.JSONArray;
 
 import com.buglabs.bug.swarm.connector.Configuration.Protocol;
 import com.buglabs.bug.swarm.connector.osgi.OSGiHelper;
+import com.buglabs.bug.swarm.connector.ws.ISwarmResourcesClient;
 import com.buglabs.bug.swarm.connector.ws.ISwarmResourcesClient.MemberType;
 import com.buglabs.bug.swarm.connector.ws.ISwarmClient;
 import com.buglabs.bug.swarm.connector.ws.SwarmResourceModel;
 import com.buglabs.bug.swarm.connector.ws.SwarmModel;
 import com.buglabs.bug.swarm.connector.ws.SwarmWSClient;
+import com.buglabs.bug.swarm.connector.ws.SwarmWSResponse;
 import com.buglabs.bug.swarm.connector.xmpp.SwarmXMPPClient;
 import com.buglabs.bug.swarm.connector.xmpp.JSONElementCreator;
 
@@ -24,137 +27,174 @@ import com.buglabs.bug.swarm.connector.xmpp.JSONElementCreator;
  * See RMI http://redmine/issues/2312
  * 
  * @author kgilmer
- *
+ * 
  */
 public class BasicConnectivityTests extends TestCase {
 
-	
 	protected MultiUserChat swarmRoom;
 	protected ChatManager chatManager;
-	
+	private SwarmWSClient wsClient;
+	private SwarmXMPPClient xmppClient;
+
+	public void testConnectWSClient() {
+		wsClient = new SwarmWSClient(AccountConfig.getConfiguration().getHostname(Protocol.HTTP), AccountConfig.getConfiguration()
+				.getAPIKey());
+
+		assertTrue(wsClient.isValid() == null);
+	}
+
+	public void testConnectXMPPClient() throws IOException, XMPPException {
+		xmppClient = new SwarmXMPPClient(AccountConfig.getXmppConfiguration());
+		xmppClient.connect();
+
+		assertTrue(xmppClient.isConnected());
+		assertTrue(xmppClient.getConnection() != null);
+		assertTrue(xmppClient.getResource() != null);
+		assertTrue(xmppClient.getHostname() != null);
+		assertTrue(xmppClient.getUsername() != null);
+	}
+
 	/**
-	 * @throws Exception 
+	 * @throws Exception
 	 * 
 	 */
 	public void testConnectToSwarmServer() throws Exception {
-		/*
-		 * 1. Get the list of swarms where the device is a member. (through the Rest API)
-		 * 2. Get the list of consumer members of every swarm returned by step 1. (through the Rest API)
-		 */
-		
-		ISwarmClient wsClient = new SwarmWSClient(AccountConfig.getConfiguration().getHostname(Protocol.HTTP), AccountConfig.getConfiguration().getAPIKey());
-		
-		assertTrue(wsClient.isValid() == null);
+		testConnectWSClient();
+		testConnectXMPPClient();
+	}
+
+	/**
+	 * Join a pre-existing swarm
+	 * 
+	 * @throws Exception
+	 */
+	public void testJoinMemberSwarms() throws Exception {
+		testConnectToSwarmServer();
+
 		List<SwarmModel> allSwarms = wsClient.getSwarmResourceClient().getSwarmsByMember(AccountConfig.getConfiguration().getResource());
-		
-        /*
-		 * 3. Join to swarms returned by step 1. (xmpp)
-		 */		
-		SwarmXMPPClient xmppClient = new SwarmXMPPClient(AccountConfig.getXmppConfiguration());
-		xmppClient.connect();
-		
-		assertTrue(xmppClient.isConnected());
-		assertTrue(xmppClient.getConnection() != null);
-		
+
 		List<SwarmModel> connectedSwarms = new ArrayList<SwarmModel>();
-		for (SwarmModel swarm: allSwarms) {
+		for (SwarmModel swarm : allSwarms) {
 			try {
-				 xmppClient.joinSwarm(swarm.getId());
-				 connectedSwarms.add(swarm);
-				 System.out.println("Joined swarm: " + swarm.getId());
+				xmppClient.joinSwarm(swarm.getId());
+				connectedSwarms.add(swarm);
+				System.out.println("Joined swarm: " + swarm.getId());
 			} catch (XMPPException e) {
-				//Some swarm joins are failing, not sure yet if this is valid or invalid.  For now assume it's
-				//ok for some joins to fail and proceed with test.
-				//TODO: confirm with Camilo this is alright.
-				
+				// Some swarm joins are failing, not sure yet if this is valid
+				// or invalid. For now assume it's
+				// ok for some joins to fail and proceed with test.
+				// TODO: confirm with Camilo this is alright.
+
 				System.err.println("Failed to join swarm " + swarm.getName() + ": " + e.getMessage());
 			}
 		}
-		
+
 		assertTrue(connectedSwarms.size() > 0);
-		
-        /* 
-		 * 4. Advertise device capabilities (services and feeds) only to consumer members that have
+
+		/*
+		 * 4. Advertise device capabilities (services and feeds) only to
+		 * consumer members that have
 		 */
-		
+
 		OSGiHelper osgi = OSGiHelper.getRef();
-		
-		for (SwarmModel swarm: connectedSwarms) 
-			for (SwarmResourceModel member: swarm.getMembers()) {
+
+		for (SwarmModel swarm : connectedSwarms)
+			for (SwarmResourceModel member : swarm.getMembers()) {
 				System.out.println("Looking at member " + member.getUserId() + " in swarm: " + swarm.getId());
-				
-				if (member.getType() == MemberType.CONSUMER &&  xmppClient.isPresent(swarm.getId(), member.getUserId())) {
+
+				if (member.getType() == MemberType.CONSUMER && xmppClient.isPresent(swarm.getId(), member.getUserId())) {
 					JSONArray advertisement = JSONElementCreator.createFeedArray(osgi.getBUGFeeds());
-					System.out.println("Sending advertisement " + advertisement + " to member " + member.getUserId() + " in swarm: " + swarm.getId());
-					
-					xmppClient.advertise(
-							swarm.getId(), 
-							member.getUserId(), 
-							advertisement);
+					System.out.println("Announcing " + advertisement + " to swarm: " + swarm.getId());
+					xmppClient.announce(swarm.getId(), advertisement);
 				}
 			}
-		
-		//At this point we should be connected to the swarm server, verify.
+
+		// At this point we should be connected to the swarm server, verify.
 		for (SwarmModel swarm : connectedSwarms) {
 			SwarmModel sd = wsClient.get(swarm.getId());
-			
-			//Check to see that I am a member of all swarms that I should be
+
+			// Check to see that I am a member of all swarms that I should be
 			boolean iAmAMember = false;
-			for (SwarmResourceModel member: sd.getMembers()) {
+			for (SwarmResourceModel member : sd.getMembers()) {
 				System.out.println("Checking if  " + member.getUserId() + " belongs to " + swarm.getId());
-				
+
 				if (member.getUserId().equals(AccountConfig.getConfiguration().getUsername())) {
-						iAmAMember = true;
-						System.out.println("Confirmed that " + member.getUserId() + " belongs to " + swarm.getId());
+					iAmAMember = true;
+					System.out.println("Confirmed that " + member.getUserId() + " belongs to " + swarm.getId());
 				}
 			}
-				
+
 			assertTrue(iAmAMember);
 		}
-			
+
 	}
-	
-	/**
-	 * Join a pre-existing swarm
-	 */
-	public void testJoinAvailableSwarm() {
-		
-	}
-	
+
 	/**
 	 * Create and join a new swarm.
 	 * 
 	 * See https://github.com/buglabs/bugswarm/wiki/Swarms-API
+	 * @throws Exception 
 	 */
-	public void testJoinNewSwarm() {
+	public void testJoinNewSwarm() throws Exception {
+		// Create a new swarm w ws api
+		testConnectToSwarmServer();
+		assertNotNull(wsClient);
+		assertNotNull(xmppClient);
 		
+		String id = wsClient.create(AccountConfig.generateRandomSwarmName(), true, "A test swarm");
+		assertNotNull(id);
+		assertTrue(id.length() > 0);
+		
+		// Add my xmpp client as a member
+		ISwarmResourcesClient resClient = wsClient.getSwarmResourceClient();
+		assertNotNull(resClient);
+		SwarmWSResponse response = resClient.add(id, ISwarmResourcesClient.MemberType.PRODUCER, xmppClient.getUsername() , xmppClient.getResource());
+		assertFalse(response.isError());
+		
+		// Create the 'web' resource, this will be used later w/ feed api
+		response = resClient.add(id, ISwarmResourcesClient.MemberType.CONSUMER, xmppClient.getUsername() , "web");
+		assertFalse(response.isError());
+		
+		// Confirm swarm has two members
+		SwarmModel swarmInfo = wsClient.get(id);
+		assertTrue(swarmInfo.getMembers().size() == 2);
+		
+		// Join the swarm w/ the xmpp client
+		xmppClient.joinSwarm(id);
+		assertTrue(xmppClient.isPresent(id, xmppClient.getUsername()));
+		
+		xmppClient.disconnect();
+		xmppClient = null;
+		
+		wsClient = null;
 	}
-	
+
 	/**
 	 * Respond to a swarm server admin message over chat for content.
 	 */
 	public void testRespondToXMPPadminChatMessage() {
-		
+
 	}
-	
+
 	/**
 	 * Respond to MUC invitation request from swarm server.
 	 */
 	public void testRespondToXMPPMUCRoomInvitation() {
-		
+
 	}
-	
+
 	/**
-	 * Respond to swarm server request for binary content with HTTP POST of requested data.
+	 * Respond to swarm server request for binary content with HTTP POST of
+	 * requested data.
 	 */
 	public void testRespondToXMPPBinaryChatMessage() {
-		
+
 	}
-	
+
 	/**
 	 * Respond to a swarm server message over chat for content.
 	 */
 	public void testRespondToXMPPMUCChatMessage() {
-		
+
 	}
 }

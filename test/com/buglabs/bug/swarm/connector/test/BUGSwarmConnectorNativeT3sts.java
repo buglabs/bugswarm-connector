7c946dd3001290961081d4ffbeed5a83d8e699aa
Ken Gilmer <kgilmer@gmail.com>
2011-07-01 16:55:11 +0900

    bugswarm-connector: more native test work.

--- test/com/buglabs/bug/swarm/connector/test/BUGSwarmConnectorNativeT3sts.java
+++ test/com/buglabs/bug/swarm/connector/test/BUGSwarmConnectorNativeT3sts.java
@@ -7,6 +7,8 @@ import junit.framework.TestCase;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
+import com.buglabs.bug.swarm.connector.ws.SwarmWSClient;
+
 /**
  * Tests the high-level BUGSwarmConnector class in regards to feeds.
  * 
@@ -43,7 +45,19 @@ public class BUGSwarmConnectorNativeT3sts extends TestCase {
 	 * Determine if system properties have enough information to run tests.
 	 */
 	public void testSystemBUGSwarmPropertiesAvailable() {
+		assertNotNull(System.getProperty("com.buglabs.bugswarm.hostname"));
 		assertNotNull(System.getProperty("com.buglabs.bugswarm.apikey"));
 		assertNotNull(System.getProperty("com.buglabs.bugswarm.username"));
+	}
+	
+	/**
+	 * Create a swarm, associate this device to the swarm.
+	 */
+	public void testCreateAssociateSwarmToBUG() {
+		String host = System.getProperty("com.buglabs.bugswarm.hostname");
+		String apikey = System.getProperty("com.buglabs.bugswarm.apikey");
+		SwarmWSClient wsClient = new SwarmWSClient("http://ws." + host, apikey);
+		
+		assertTrue(wsClient.isValid() == null);
 	}
 }

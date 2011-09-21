2609ee34dd07de7035d6dcc581e22e04927bec06
Ken Gilmer <kgilmer@gmail.com>
2011-09-21 11:59:10 +0900

    bugswarm-connector: only download dependencies if they don't exist.

--- build-connector.sh
+++ build-connector.sh
@@ -32,23 +32,23 @@ if [ ! -f $DEPS_DIR/osgi.core.jar ]; then
 fi
 
 if [ ! -f $DEPS_DIR/osgi.cmpn.jar ]; then
-	wget -P $DEPS_DIR http://www.osgi.org/download/r4v42/osgi.cmpn.jar
+	wget -P $DEPS_DIR -nc http://www.osgi.org/download/r4v42/osgi.cmpn.jar
 fi
 
 if [ ! -f $DEPS_DIR/junit-dep-4.9b2.jar ]; then
-	wget --no-check-certificate -P $DEPS_DIR https://github.com/downloads/KentBeck/junit/junit-dep-4.9b2.jar
+	wget --no-check-certificate -P $DEPS_DIR -nc https://github.com/downloads/KentBeck/junit/junit-dep-4.9b2.jar
 fi 
 
 if [ ! -f $DEPS_DIR/hamcrest-core-1.3.0RC2.jar ]; then
-	wget --no-check-certificate -P $DEPS_DIR http://hamcrest.googlecode.com/files/hamcrest-core-1.3.0RC2.jar
+	wget --no-check-certificate -P $DEPS_DIR -nc http://hamcrest.googlecode.com/files/hamcrest-core-1.3.0RC2.jar
 fi
 
 if [ ! -f $DEPS_DIR/javax.servlet_2.3.0.v200806031603.jar ]; then
-	wget --no-check-certificate -O $DEPS_DIR/javax.servlet_2.3.0.v200806031603.jar "http://www.eclipse.org/downloads/download.php?r=1&file=/tools/orbit/downloads/drops/R20100519200754/bundles/javax.servlet_2.3.0.v200806031603.jar"
+	wget --no-check-certificate -nc -O $DEPS_DIR/javax.servlet_2.3.0.v200806031603.jar "http://www.eclipse.org/downloads/download.php?r=1&file=/tools/orbit/downloads/drops/R20100519200754/bundles/javax.servlet_2.3.0.v200806031603.jar"
 fi
 
 if [ ! -f $DEPS_DIR/commons-io-2.0.1.jar ]; then
-	wget --no-check-certificate -O /tmp/commons-io-2.0.1-bin.zip "http://www.meisei-u.ac.jp/mirror/apache/dist/commons/io/binaries/commons-io-2.0.1-bin.zip"
+	wget --no-check-certificate -O /tmp/commons-io-2.0.1-bin.zip -nc "http://www.meisei-u.ac.jp/mirror/apache/dist/commons/io/binaries/commons-io-2.0.1-bin.zip"
 	unzip -d /tmp /tmp/commons-io-2.0.1-bin.zip
 	cp /tmp/commons-io-2.0.1/commons-io-2.0.1.jar $DEPS_DIR
 	rm -Rf /tmp/commons-io-2.0.1*

cc38e1e1dfeb8fb6001ec970ce76461ce078a8ba
Ken Gilmer <kgilmer@gmail.com>
2011-10-20 11:38:55 +0900

    bugswarm-connector: add http response 204 to handled responses.

--- com.buglabs.bug.swarm.restclient/src/com/buglabs/bug/swarm/connector/ws/SwarmWSResponse.java
+++ com.buglabs.bug.swarm.restclient/src/com/buglabs/bug/swarm/connector/ws/SwarmWSResponse.java
@@ -11,16 +11,18 @@ package com.buglabs.bug.swarm.connector.ws;
 public enum SwarmWSResponse {
 	/*
 	 * These codes and descriptions were copied/pasted from specification.
-	 */
-	R200(200, "Request was received and handled without errors."), R201(201, "The entity was created successfully."), R400(
-			400,
-			"Bad Request. There was something wrong with the specified data. It may be an unrecognizable format or a missing field. The platform will provide a descriptive message with the application error code for easier debugging."), R401(
-			401, "Unauthorized. You may not have specified the HTTP header containing your API Key."), R403(403,
-			"Forbidden. The resource may exists but you don't have access to it due to application constraints."), R404(404,
-			"Not found. The specified url does not exist in the server or the entity itself does not exist in our database."), R409(
-			409,
-			"Conflict. There was a conflict with the request. i.e: trying to delete an API Key specified in the x-bugswarmapikey header of the same request."), R500(
-			500, "Internal Error. Something went wrong with our server. If you got this error please notify it to our development team.");
+	 * @formatter:off
+	 */	
+	R200(200, "Request was received and handled without errors."), 
+	R201(201, "The entity was created successfully."), 
+	R204(204, "The entity was created successfully, no content returned."), 
+	R400(400, "Bad Request. There was something wrong with the specified data. It may be an unrecognizable format or a missing field. The platform will provide a descriptive message with the application error code for easier debugging."), 
+	R401(401, "Unauthorized. You may not have specified the HTTP header containing your API Key."), 
+	R403(403, "Forbidden. The resource may exists but you don't have access to it due to application constraints."), 
+	R404(404, "Not found. The specified url does not exist in the server or the entity itself does not exist in our database."), 
+	R409(409, "Conflict. There was a conflict with the request. i.e: trying to delete an API Key specified in the x-bugswarmapikey header of the same request."), 
+	R500(500, "Internal Error. Something went wrong with our server. If you got this error please notify it to our development team.");
+	// @formatter:on
 
 	private final int code;
 	private final String message;

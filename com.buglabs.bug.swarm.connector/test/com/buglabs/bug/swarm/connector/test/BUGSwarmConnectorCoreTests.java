0f7adad7ada91c82bd695b3475f1a465a776c323
Ken Gilmer <kgilmer@gmail.com>
2011-11-18 11:12:52 +0900

    bugswarm-connector: moved readme and added start for osgi test automation

---
+++ com.buglabs.bug.swarm.connector/test/com/buglabs/bug/swarm/connector/test/BUGSwarmConnectorCoreTests.java
@@ -0,0 +1,15 @@
+package com.buglabs.bug.swarm.connector.test;
+
+import junit.framework.TestCase;
+
+public class BUGSwarmConnectorCoreTests extends TestCase {
+
+	/**
+	 *  it('should allow to connect as a producer resource only'
+ 	 * it('should allow to connect as a consumer resource only'
+  		it('should allow to connect as producer and consumer'
+ 		it('should connect, join and send messages to more than one swarm'
+ 		it('should not lose messages if connection goes down'
+ 		it('should re-connect if connection goes down'
+	 */
+}
